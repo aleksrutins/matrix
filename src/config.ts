b88d72207df16d0943eee96338715771834d2267
Aleks Rutins <aleks@rutins.com>
2022-09-28 18:12:03 -0400

    fix lint

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 export type Config = {
-	projectName: string,
-	configPath: string,
-	move: {
-		from: string,
-		to: string,
-	}[],
-	configRegex: {
-		find: string,
-		replace: string,
-	},
-	configurations: Record<string, string>,
-	targets: Record<string, string[]>
+  projectName: string
+  configPath: string
+  move: {
+    from: string
+    to: string
+  }[]
+  configRegex: {
+    find: string
+    replace: string
+  }
+  configurations: Record<string, string>
+  targets: Record<string, string[]>
 }
